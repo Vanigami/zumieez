f4c644825d9b478fb9e92644da1cafdaa509c252
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-10-30 13:48:28 -0400

    add template react import and export to navbar and import to home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <h1>
+                Nav
+            </h1>
+        )
+    }
+}
