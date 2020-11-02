8fcb83e6f4a945e0cb9a6824e507050502e260b6
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-11-02 18:55:27 -0500

    create reducer function to take in parameters of state and action

---
+++ src/reducers/shoppingCartReducer.jsx
@@ -0,0 +1,15 @@
+const initState = {
+    items: [
+       
+        {id:1, title:"Gold Bears Gummi Candy",price:1.79}
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const shoppingCartReducer= (state = initState,action)=>{
+    
+    return state;
+
+}
+export default shoppingCartReducer;
