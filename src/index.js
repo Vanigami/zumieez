38eaa033d9483647a51f06779af54516dc7001c8
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-11-02 18:44:34 -0500

    add imports for createStore from redux for state and provider to store as props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux"
+import shoppingCartReducer from "./reducers/shoppingCartReducer"
 import App from './App';
 
 
