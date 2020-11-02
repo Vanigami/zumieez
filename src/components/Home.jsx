380ca92a84afb06ca9bd033c7bd9cebb602c851a
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-11-02 18:39:52 -0500

    create shoppingcart reducer file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import Navbar from "./Navbar"
-import "../App.css"
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <button class="hamburger hamburger--slider" type="button">
-                    <span class="hamburger-box">
-                        <span class="hamburger-inner"></span>
-                    </span>
-                </button>  
-             {/* <Navbar /> */}
+             <Navbar />
             </div>
         )
     }
