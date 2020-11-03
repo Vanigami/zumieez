e8192b87a159ffd27b8c83cdc945ffada33a56b1
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-11-02 19:12:55 -0500

    add map state to properties to render card for items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Navbar from "./Navbar"
 
-export default class Home extends Component {
+class Home extends Component {
     render() {
-        return (
-            <div>
-             <Navbar />
+        let itemList = this.props.items.map(item=>{
+            return(
+                <div className="card" key={item.id}>
+                    <div className="card-image">
+                        <span className="card-title">{item.title}</span>
+                        <span to="/" className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></span>
+                    </div>
+                </div>
+            )
+        })
+        return(
+            <div className="container">
+                <h3 className="center">Candies</h3>
+                <div className="box">{itemList}</div>
             </div>
         )
     }
 }
+const mapStateToProps = (state)=>{
+    return{
+        items: state.items
+    }
+}
+export default connect(mapStateToProps)(Home)
